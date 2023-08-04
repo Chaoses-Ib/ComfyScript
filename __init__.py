d852c2ec4f408481e9b22758b014e90b82d01426
Chaoses-Ib <Chaos-es@outlook.com>
2023-08-05 03:27:09 +0800

    feat(image): copy LoadImage

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .image import *
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+NODE_CLASS_MAPPINGS = {
+    "LoadImageFromPath": LoadImageFromPath
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadImageFromPath": "Load Image From Path"
+}
