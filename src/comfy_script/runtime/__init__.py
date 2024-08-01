2cbd9dc909ecf30c5eff7ff369016467b873a6d7
Chaoses-Ib <Chaos-es@outlook.com>
2024-08-02 04:39:07 +0800

    feat(runtime): update ComfyUIArgs docs (#64)

--- src/comfy_script/runtime/__init__.py
+++ src/comfy_script/runtime/__init__.py
@@ -72,17 +72,24 @@ class ComfyUIArgs:
 
     argv: list[str]
     '''```sh
-    usage: [-h] [--listen [IP]] [--port PORT] [--enable-cors-header [ORIGIN]] [--max-upload-size MAX_UPLOAD_SIZE] [--extra-model-paths-config PATH [PATH ...]] [--output-directory OUTPUT_DIRECTORY]
-                [--temp-directory TEMP_DIRECTORY] [--input-directory INPUT_DIRECTORY] [--auto-launch] [--disable-auto-launch] [--cuda-device DEVICE_ID] [--cuda-malloc | --disable-cuda-malloc]
-                [--dont-upcast-attention] [--force-fp32 | --force-fp16] [--bf16-unet | --fp16-unet | --fp8_e4m3fn-unet | --fp8_e5m2-unet] [--fp16-vae | --fp32-vae | --bf16-vae] [--cpu-vae]
-                [--fp8_e4m3fn-text-enc | --fp8_e5m2-text-enc | --fp16-text-enc | --fp32-text-enc] [--directml [DIRECTML_DEVICE]] [--disable-ipex-optimize] [--preview-method [none,auto,latent2rgb,taesd]]    
-                [--use-split-cross-attention | --use-quad-cross-attention | --use-pytorch-cross-attention] [--disable-xformers] [--gpu-only | --highvram | --normalvram | --lowvram | --novram | --cpu]       
-                [--disable-smart-memory] [--deterministic] [--dont-print-server] [--quick-test-for-ci] [--windows-standalone-build] [--disable-metadata] [--multi-user]
+    usage: [-h] [--listen [IP]] [--port PORT] [--tls-keyfile TLS_KEYFILE] [--tls-certfile TLS_CERTFILE] [--enable-cors-header [ORIGIN]] [--max-upload-size MAX_UPLOAD_SIZE]
+                [--extra-model-paths-config PATH [PATH ...]] [--output-directory OUTPUT_DIRECTORY] [--temp-directory TEMP_DIRECTORY] [--input-directory INPUT_DIRECTORY] [--auto-launch]
+                [--disable-auto-launch] [--cuda-device DEVICE_ID] [--cuda-malloc | --disable-cuda-malloc] [--force-fp32 | --force-fp16]
+                [--bf16-unet | --fp16-unet | --fp8_e4m3fn-unet | --fp8_e5m2-unet] [--fp16-vae | --fp32-vae | --bf16-vae] [--cpu-vae]
+                [--fp8_e4m3fn-text-enc | --fp8_e5m2-text-enc | --fp16-text-enc | --fp32-text-enc] [--force-channels-last] [--directml [DIRECTML_DEVICE]] [--disable-ipex-optimize]
+                [--preview-method [none,auto,latent2rgb,taesd]] [--use-split-cross-attention | --use-quad-cross-attention | --use-pytorch-cross-attention] [--disable-xformers]
+                [--force-upcast-attention | --dont-upcast-attention] [--gpu-only | --highvram | --normalvram | --lowvram | --novram | --cpu] [--default-hashing-function {md5,sha1,sha256,sha512}]  
+                [--disable-smart-memory] [--deterministic] [--dont-print-server] [--quick-test-for-ci] [--windows-standalone-build] [--disable-metadata] [--disable-all-custom-nodes]
+                [--multi-user] [--verbose] [--front-end-version FRONT_END_VERSION] [--front-end-root FRONT_END_ROOT]
 
     options:
     -h, --help            show this help message and exit
     --listen [IP]         Specify the IP address to listen on (default: 127.0.0.1). If --listen is provided without an argument, it defaults to 0.0.0.0. (listens on all)
     --port PORT           Set the listen port.
+    --tls-keyfile TLS_KEYFILE
+                            Path to TLS (SSL) key file. Enables TLS, makes app accessible at https://... requires --tls-certfile to function
+    --tls-certfile TLS_CERTFILE
+                            Path to TLS (SSL) certificate file. Enables TLS, makes app accessible at https://... requires --tls-keyfile to function
     --enable-cors-header [ORIGIN]
                             Enable CORS (Cross-Origin Resource Sharing) with optional origin or allow all with default '*'.
     --max-upload-size MAX_UPLOAD_SIZE
@@ -103,8 +110,6 @@ class ComfyUIArgs:
     --cuda-malloc         Enable cudaMallocAsync (enabled by default for torch 2.0 and up).
     --disable-cuda-malloc
                             Disable cudaMallocAsync.
-    --dont-upcast-attention
-                            Disable upcasting of attention. Can boost speed but increase the chances of black images.
     --force-fp32          Force fp32 (If this makes your GPU work better please report it).
     --force-fp16          Force fp16.
     --bf16-unet           Run the UNET in bf16. This should only be used for testing stuff.
@@ -120,6 +125,8 @@ class ComfyUIArgs:
     --fp8_e5m2-text-enc   Store text encoder weights in fp8 (e5m2 variant).
     --fp16-text-enc       Store text encoder weights in fp16.
     --fp32-text-enc       Store text encoder weights in fp32.
+    --force-channels-last
+                            Force channels last format when inferencing the models.
     --directml [DIRECTML_DEVICE]
                             Use torch-directml.
     --disable-ipex-optimize
@@ -133,21 +140,51 @@ class ComfyUIArgs:
     --use-pytorch-cross-attention
                             Use the new pytorch 2.0 cross attention function.
     --disable-xformers    Disable xformers.
+    --force-upcast-attention
+                            Force enable attention upcasting, please report if it fixes black images.
+    --dont-upcast-attention
+                            Disable all upcasting of attention. Should be unnecessary except for debugging.
     --gpu-only            Store and run everything (text encoders/CLIP models, etc... on the GPU).
     --highvram            By default models will be unloaded to CPU memory after being used. This option keeps them in GPU memory.
     --normalvram          Used to force normal vram use if lowvram gets automatically enabled.
     --lowvram             Split the unet in parts to use less vram.
     --novram              When lowvram isn't enough.
     --cpu                 To use the CPU for everything (slow).
+    --default-hashing-function {md5,sha1,sha256,sha512}
+                            Allows you to choose the hash function to use for duplicate filename / contents comparison. Default is sha256.
     --disable-smart-memory
                             Force ComfyUI to agressively offload to regular ram instead of keeping models in vram when it can.
+    --lowvram             Split the unet in parts to use less vram.
+    --novram              When lowvram isn't enough.
+    --cpu                 To use the CPU for everything (slow).
+    --default-hashing-function {md5,sha1,sha256,sha512}
+                            Allows you to choose the hash function to use for duplicate filename / contents comparison. Default is sha256.
+    --disable-smart-memory
+                            Force ComfyUI to agressively offload to regular ram instead of keeping models in vram when it can.
+    --deterministic       Make pytorch use slower deterministic algorithms when it can. Note that this might not make images deterministic in all cases.
+    --default-hashing-function {md5,sha1,sha256,sha512}
+                            Allows you to choose the hash function to use for duplicate filename / contents comparison. Default is sha256.
+    --disable-smart-memory
+                            Force ComfyUI to agressively offload to regular ram instead of keeping models in vram when it can.
+    --deterministic       Make pytorch use slower deterministic algorithms when it can. Note that this might not make images deterministic in all cases.
+    --disable-smart-memory
+                            Force ComfyUI to agressively offload to regular ram instead of keeping models in vram when it can.
+    --deterministic       Make pytorch use slower deterministic algorithms when it can. Note that this might not make images deterministic in all cases.
     --deterministic       Make pytorch use slower deterministic algorithms when it can. Note that this might not make images deterministic in all cases.
     --dont-print-server   Don't print server output.
     --quick-test-for-ci   Quick test for CI.
     --windows-standalone-build
-                            Windows standalone build: Enable convenient things that most people using the standalone windows build will probably enjoy (like auto opening the page on startup).
+                            Windows standalone build: Enable convenient things that most people using the standalone windows build will probably enjoy (like auto opening the page on startup).        
     --disable-metadata    Disable saving prompt metadata in files.
+    --disable-all-custom-nodes
+                            Disable loading all custom nodes.
     --multi-user          Enables per-user storage.
+    --verbose             Enables more debug prints.
+    --front-end-version FRONT_END_VERSION
+                            Specifies the version of the frontend to be used. This command needs internet connectivity to query and download available frontend implementations from GitHub releases.  
+                            The version string should be in the format of: [repoOwner]/[repoName]@[version] where version is one of: "latest" or a valid version number (e.g. "1.0.0")
+    --front-end-root FRONT_END_ROOT
+                            The local filesystem path to the directory where the frontend is located. Overrides --front-end-version.
     ```'''
 
     def __init__(self, *argv: str):
